1d023a529010264fe1ec2a50d0eb3179a55137cc
zestsystem <mikeyim@unitetheculture.org>
2024-07-24 13:38:37 -0700

    load consumer and producer

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,26 @@
 import Fastify from "fastify";
+import autoLoad from "@fastify/autoload";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+// @ts-ignore
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const fastify = Fastify({
     logger: true,
 });
+
+fastify.register(autoLoad, {
+    dir: join(__dirname, 'plugins'),
+    ignoreFilter: (path) => path.endsWith('test.js') || path.endsWith('test.ts'),
+});
+
+fastify.register(autoLoad, {
+    dir: join(__dirname, 'consumers'),
+    ignoreFilter: (path) => path.endsWith('test.js') || path.endsWith('test.ts'),
+});
+
 
 fastify.get("/", async (request, reply) => {
     return { hello: "world" };
